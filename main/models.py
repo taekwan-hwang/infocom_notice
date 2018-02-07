d97fc249aef5fbf0070c889bc5fa54d9fea04f5c
taekwan-hwang <xorhks0005@gmail.com>
2018-02-07 04:13:10 +0000

    add uid, isSent fields in models.py

--- main/models.py
+++ main/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Notice(models.Model):
+	uid=models.IntegerField(primary_key=True)
 	url=models.CharField(max_length=100, null=False)#path
 	title=models.CharField(max_length=100, null=False)#name
 	posted_date=models.DateField()
 	added_in_db_date=models.DateField(auto_now_add=True)
 	views=models.IntegerField()#조회수
+	isSent=models.BooleanField(default=False)
