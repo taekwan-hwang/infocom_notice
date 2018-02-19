6506a4845b6572c980d09366ea1d8fe1f416f9f8
taekwan-hwang <xorhks0005@gmail.com>
2018-02-19 05:13:34 +0000

    adjust kakao's api call rule

--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-
+from django.test import Client
+from rest_framework.parsers import JSONParser
+from django.utils.six import BytesIO
 # Create your tests here.
 
 class SenderTest(TestCase):
@@ -22,3 +24,35 @@ class CrawlerTest(TestCase):
 			notice.isSent=True
 			notice.save()
 		print(crawler.get_new_notices())
+
+class KakaoAdjustTest(TestCase):
+	def _check_status_code(self, response, code=200):
+		if response.status_code != code:
+			raise ValueError('unexpected status code')
+			
+	def testKbd(self):
+		client=Client()
+		response=client.get('/main/keyboard')
+		self._check_status_code(response)
+		json=BytesIO(response.content)
+		content=JSONParser().parse(json)
+		if content['type'] != 'text':
+			raise ValueError('unexpected text')
+		
+	def testMsg(self):
+		client=Client()
+		response = client.post('/main/message', {'user_key':'test_key', 'type':'text', 'content':'hi'})
+		self._check_status_code(response)
+		json=BytesIO(response.content)
+		content=JSONParser().parse(json)
+		if content['message']['text'] != 'hi':
+			raise ValueError('unexpected text')
+			
+	def testPassRequest(self):
+		client=Client()
+		response=client.post('/main/friend', {'user_key':'test_key'})
+		self._check_status_code(response)
+		response=client.delete('/main/friend', {'user_key':'test_key'})
+		self._check_status_code(response)
+		response=client.delete('/main/chat_room', {'user_key':'test_key'})
+		
