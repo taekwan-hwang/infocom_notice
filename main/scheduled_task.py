a42b1dcde2eb057091ccbff4853d6dc58a51d41c
taekwan-hwang <xorhks0005@gmail.com>
2018-02-21 02:26:36 +0000

    change msg how to send notices

--- main/scheduled_task.py
+++ main/scheduled_task.py
@@ -3,6 +3,7 @@ from main.sender.sender import KakaoSender
 
 def send_new_msg_to_kakao():
 	crawler=InfocomCrawler()
-	msg=crawler.get_new_notices()
+	notices=crawler.get_new_notices()
+	msg=Notice.to_msg(notices)
 	sender=KakaoSender()
 	sender.send_msg(msg)
