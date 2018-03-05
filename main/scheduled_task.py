b343d7d1ff0d1d4bb67fe415da03538eb85680f0
taekwan-hwang <xorhks0005@gmail.com>
2018-03-05 02:30:15 +0000

    2번 보내지는 문제 수정

--- main/scheduled_task.py
+++ main/scheduled_task.py
@@ -1,9 +1,11 @@
 """
 주기적으로 실행해야 할 함수들
 """
+import logging
 from main.crawler.infocom_crawler import InfocomCrawler
 from main.sender.sender import KakaoSender
 from main.util.msg_handler import to_msg, set_sent_status
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 def send_new_msg_to_kakao():
@@ -18,3 +20,12 @@ def send_new_msg_to_kakao():
     sender = KakaoSender()
     sender.send_msg(msg)
     set_sent_status(notices)
+
+
+logging.basicConfig(filename='./test.log', level=logging.INFO)
+# apscheduler에서 일어나는 모든 정보는 warning이 아니면 무시
+logging.getLogger("apscheduler").setLevel(logging.WARNING)
+scheduler = BackgroundScheduler()
+# interval 형식으로 작동하며, 1시간마다 send_new_msg_to_kakao 실행
+scheduler.add_job(send_new_msg_to_kakao, 'interval', seconds=10, max_instances=1)
+scheduler.start()
